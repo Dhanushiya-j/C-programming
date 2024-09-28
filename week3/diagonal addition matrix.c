9908d9bbfd8e3b2164ab8be9df296d447f3267ac
Dhanushiya-j <dhanushiyajershilin.2006@gmail.com>
2024-09-28 17:42:54 +0530

    Update and rename diagonal matrix.c to diagonal addition matrix.c

--- week3/diagonal addition matrix.c
+++ week3/diagonal addition matrix.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main ()
 {
-int i,j,matnum[5][5],m,n;
+int i,j,matnum[5][5],m,n,total=0;
 printf("row:");
 scanf("%d",&m);
 printf("column:");
@@ -12,17 +12,18 @@ printf("column:");
       { 
           printf("enter the number[%d][%d]",i,j);
           scanf("%d",&matnum[i][j]);
-
       }
-
    }
        for (i=0;i<m;i++)
    {
        for (j=0;j<n;j++)
       {      
           if(i==j)
-           printf("diagonal element:%d",matnum[i][j]);
-         }
-   }
+        total=total+matnum[i][j];
+       }
+  }
+    printf("diagonal element addition:%d",total);
+}
+
    
 }
