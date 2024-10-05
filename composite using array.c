04b1a799a5d8a3d0e8793d2fcab19fdec8090c4c
Dhanushiya-j <dhanushiyajershilin.2006@gmail.com>
2024-10-05 20:50:21 +0530

    Create composite using array.c

---
+++ composite using array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main() 
+{
+    int ar[20],i,j,n;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {printf("enter the number %d:",i+1);
+        scanf("%d",&ar[i]);
+    }
+    for(i=0;i<n;i++)
+    { for(j=2;j<ar[i]/2;j++)
+        if((ar[i])%(j)==0)
+        {
+        printf("%d is composite\n",ar[i]);
+        break;
+       
+        }
+        
+        
+    }
+}
