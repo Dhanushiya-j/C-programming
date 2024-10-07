f96a0c8ea11a5e6b8d5ce010abb308a8a4e01640
Dhanushiya-j <dhanushiyajershilin.2006@gmail.com>
2024-10-07 16:14:44 +0530

    Update comparray.c

--- week4/comparray.c
+++ week4/comparray.c
@@ -9,8 +9,8 @@ void main()
     {printf("enter the number %d:",i+1);
         scanf("%d",&ar[i]);
     }
-    for(i=0;i<n;i++)
-    { for(j=2;j<ar[i]/2;j++)
+    for(i=2;i<n;i++)
+    { for(j=2;j<=ar[i]/2;j++)
         if((ar[i])%(j)==0)
         {
         printf("%d is composite\n",ar[i]);
